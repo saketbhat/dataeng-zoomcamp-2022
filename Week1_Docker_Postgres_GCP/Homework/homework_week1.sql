3bbd828a06500432f247a9a6c7162dc366f400bf
saketbhat <saketbhat@users.noreply.github.com>
2022-01-26 15:36:12 -0500

    Update homework_week1.sql

--- Week1_Docker_Postgres_GCP/Homework/homework_week1.sql
+++ Week1_Docker_Postgres_GCP/Homework/homework_week1.sql
@@ -1,1 +1,24 @@
 
+--Question 3 : Count Records
+--How many taxi trips were there on January 15?
+--Consider only trips that started on January 15.
+
+select count(*) from yellow_taxi_data where to_char(tpep_pickup_datetime ::DATE,'MM/DD') = '01/15' -- Ans: 53024;
+
+--Question 4 : Largest tip of the day
+
+--Find the largest tip for each day.
+select max(tip_amount) as max_tip, tpep_pickup_datetime :: DATE as dt from
+	yellow_taxi_data group by tpep_pickup_datetime :: DATE; 
+  
+-- On which day it was the largest tip in January?  
+ Select * from (Select a.dt , dense_rank() over (order by a.max_tip desc) as ranking from (
+select max(tip_amount) as max_tip, tpep_pickup_datetime :: DATE as dt from yellow_taxi_data 
+	where TO_CHAR(tpep_pickup_datetime, 'MM') = '01' group by tpep_pickup_datetime :: DATE ) a ) b where b.ranking = 1 -- Ans : 2021-01-20;
+  
+ -- Question 5 : To be solved
+ 
+ 
+ -- Question 6 : To be solved
+  
+  
